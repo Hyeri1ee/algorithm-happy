702b57d3a1acc91291b69d577f9fc5e42c29e333
Hyeri1ee <haerizian10@gmail.com>
2024-11-15 21:28:08 +0900

    feat: BOJ11003

--- src/알고리즘_코딩테스트/day3/BOJ11003_최솟값찾기.java
+++ src/알고리즘_코딩테스트/day3/BOJ11003_최솟값찾기.java
@@ -1,42 +1,30 @@
 package 알고리즘_코딩테스트.day3;
-
 import java.io.*;
 import java.util.*;
-/*
-Di = 구간의 크기가 L로 정해져 있는 곳에서 최솟값
- */
 
 public class BOJ11003_최솟값찾기 {
-
-  class myDeque{
-    int index;
-    int value;
-
-    public myDeque(int index, int value) {
-      this.index = index;
-      this.value = value;
-    }
-  }
-
-  static int N,L;
-  static Deque<myDeque> dequeList[] = new Deque<>();
-  static int result[];
   public static void main(String[] args) throws IOException{
-    //입력값 받기
-    input();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-  }
-  private static void input() throws IOException {
-    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(bf.readLine());
-    N = Integer.parseInt(st.nextToken());
-    L = Integer.parseInt(st.nextToken());
-    result = new int[N];
-    st = new StringTokenizer(bf.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int l = Integer.parseInt(st.nextToken());
 
-    for (int i = 0  ; i < N;  i++){
-      A[i] =  Integer.parseInt(st.nextToken());
+    Deque<int[]> q = new ArrayDeque<>();
+    st = new StringTokenizer(br.readLine());
+    for(int i=0; i<n; i++) {
+      int num = Integer.parseInt(st.nextToken());
+      while(!q.isEmpty() && q.peekLast()[0] > num)
+        q.pollLast();
+
+      q.offer(new int[] {num,i});
+      if(q.peek()[1] < i -(l-1)) {
+        q.poll();
+      }
+      bw.write(q.peek()[0]+" ");
     }
-
+    bw.flush();
+    bw.close();
   }
 }
