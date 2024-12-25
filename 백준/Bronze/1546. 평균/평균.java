bb43bc7a3db909f11a0fa06393624d5add943027
이혜리 Hyeri Lee <108603070+Hyeri1ee@users.noreply.github.com>
2024-12-25 23:39:05 +0900

    [Bronze I] Title: 평균, Time: 100 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    int n = Integer.parseInt(bf.readLine());
+    StringTokenizer st = new StringTokenizer(bf.readLine());
+    int num[] = new int[n];
+    int max = 0;
+    for (int i = 0 ; i < n; i++){
+      int cur = Integer.parseInt(st.nextToken());
+      if (max < cur)
+          max = cur;
+      num[i] = cur;
+    }
+    //평균 구하기
+    double sum = 0;
+    for (int i = 0 ; i < n; i++){
+      sum += (num[i] * 100.0) / max;
+    }
+    System.out.println(sum/n);
+  }
+}
