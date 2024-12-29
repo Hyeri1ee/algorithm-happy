374db5f7f4fc6557c62e1151119b3f7c3d5741b0
Hyeri1ee <haerizian10@gmail.com>
2024-12-29 12:22:00 +0900

    [BOJ]10986_나머지 합

--- src/알고리즘_코딩테스트/re/re4/BOJ_10986.java
+++ src/알고리즘_코딩테스트/re/re4/BOJ_10986.java
@@ -9,43 +9,43 @@ import java.util.*;
  */
 public class BOJ_10986 {
   static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-  static int N, M;
-  static int[] arr, sum;
+  static long N, M;
+  static long[] arr, sum;
 
   public static void main(String[] args) throws IOException {
     initialize();
 
-    int count = 0;
-    int[] countArr = new int[M];
+    long count = 0;
+    long[] countArr = new long[(int) M];
 
     for (int i = 0; i < N; i++) {
       if (sum[i] == 0)
-        countArr[0] += 1;
+        countArr[0]++;
       else {//나누어 떨어지지 않은 경우
-        countArr[sum[i]] += 1;
+        countArr[(int) sum[i]]++;
       }
     }
 
     count += countArr[0];//나머지가 0인 개수
-    for (int i = 1 ; i < N/2; i++){
-      count += combination(countArr[i],countArr[N-1-i]);
+    for (int i = 0 ; i < M; i++){
+      count += combination(countArr[i]);
     }
 
     System.out.println(count);
   }
 
-  private static int combination(int i, int i1) {
-    return i * i1 / 2;
+  private static long combination(long i) {
+    return (i * (i-1)) / 2;
   }
 
   private static void initialize() throws IOException {
     StringTokenizer st = new StringTokenizer(bf.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-    arr = new int[N];
-    sum = new int[N];
+    arr = new long[(int) N];
+    sum = new long[(int) N];
     arr = Arrays.stream(bf.readLine().split(" "))
-            .mapToInt(Integer::parseInt)
+            .mapToLong(Long::parseLong)
             .toArray();
     sum[0] = arr[0] % M;
 
