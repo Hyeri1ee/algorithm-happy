6dceee1e58376756458b39941d6bfe0ccd6c5c12
이혜리 Hyeri Lee <108603070+Hyeri1ee@users.noreply.github.com>
2024-12-29 16:26:25 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 15440 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,60 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static int N;
+  static long[] arr;
+  static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+  public static void main(String[] args) throws IOException {
+    N = Integer.parseInt(bf.readLine());
+
+    arr = Arrays.stream(bf.readLine().split(" "))
+            .mapToLong(Long::parseLong)
+            .toArray();
+
+    Arrays.sort(arr);
+
+    int count = modifyCount();
+
+    System.out.println(count);
+    bf.close();
+
+  }
+
+  private static int modifyCount() {
+    int count = 0;
+
+    // N이 2 이하면 좋은 수가 될 수 없음
+    if (N <= 2) return 0;
+
+    for (int i = 0; i < N; i++) {
+      int start = 0, end = N-1;
+      long target = arr[i];
+
+      while(start < end) {
+        // 자기 자신을 제외
+        if (start == i) {
+          start++;
+          continue;
+        }
+        if (end == i) {
+          end--;
+          continue;
+        }
+
+        long sum = arr[start] + arr[end];
+        if (sum == target) {
+          count++;
+          break;
+        } else if (sum > target) {
+          end--;
+        } else {
+          start++;
+        }
+      }
+    }
+    return count;
+  }
+}
