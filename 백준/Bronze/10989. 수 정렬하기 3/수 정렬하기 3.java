865772619ae63c6420bdf34aa926e285b5255afc
이혜리 Hyeri Lee <108603070+Hyeri1ee@users.noreply.github.com>
2025-02-15 21:29:31 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1548 ms, Memory: 337084 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,32 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] arr;
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(bf.readLine());
+        int max = 0;
+        arr = new int[10001];
+        for (int i = 0; i < n; i++) {
+            arr[Integer.parseInt(bf.readLine())]++;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1 ; i< 10001; i++){
+            while(arr[i] > 0){
+                sb.append(i).append("\n");
+                arr[i]--;
+            }
+        }
+        System.out.println(sb);
+
+
+
+
+
+    }
+}
