ad70aca55f31cff6b01e8102001ef57e8a449103
이혜리 Hyeri Lee <108603070+Hyeri1ee@users.noreply.github.com>
2025-03-24 23:22:17 +0900

    [Gold III] Title: 크게 만들기, Time: 576 ms, Memory: 67172 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -0,0 +1,53 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    static int n, k;
+    static Stack<Integer> stack = new Stack<>();
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args)throws IOException {
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        int[] str = Arrays.stream(bf.readLine().split(""))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+
+        int cnt = 0;
+        for(int i = 0; i < str.length; i++){
+            while(!stack.isEmpty() && cnt < k && stack.peek() < str[i]) {
+                cnt++;
+                stack.pop();
+            }
+            stack.push(str[i]);
+        }
+
+        int left = k-cnt;
+        while(left-- > 0){
+            stack.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < n-k;i++){
+            sb.append(stack.pop().toString());
+        }
+
+        System.out.println(sb.reverse());
+
+        /*PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder()); //내림차순
+        int i = 0;
+        while(size-- > 0){
+            queue.add(str[i]);
+            i++;
+        }
+
+        */
+
+
+    }
+}
