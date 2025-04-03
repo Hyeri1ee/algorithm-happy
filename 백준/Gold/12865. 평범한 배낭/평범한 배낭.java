c5d1f0eb07ea0a724405ab0e387edb36d444a460
이혜리 Hyeri Lee <108603070+Hyeri1ee@users.noreply.github.com>
2025-04-03 23:27:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53540 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,74 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    /*
+4 7
+6 13
+4 8
+3 6
+5 12
+
+14
+     */
+    static class item{
+        int weight, value;
+
+        public item (int w, int v){
+            this.weight = w;
+            this.value = v;
+        }
+
+    }
+    static int n, k;
+    static item[] items;
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+
+        input();
+        solve();
+
+    }
+    private static void solve(){
+        int[][] dp = new int[n+1][]; //dp[i][j] = i번째까지의 물건을 고려했을 때, j무게 이하의 물건의 무게 합
+        for(int i =0 ; i <= n; i++){
+            dp[i] = new int[k+1];
+        }
+
+
+        for(int i =1 ; i <= n ;i++){
+            for(int j = 1; j < k+1 ; j++){
+               dp[i][j] = dp[i - 1][j];
+               if (items[i].weight <= j){
+                   dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-items[i].weight] + items[i].value);
+               }
+           }
+       }
+
+        System.out.println(dp[n][k]);
+
+
+
+    }
+    private static void input() throws IOException{
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        items = new item[n+1];
+
+        for(int i = 1 ; i <= n; i++){
+            st = new StringTokenizer(bf.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            items[i] = new item(w,v);
+        }
+
+        //end input
+    }
+}
