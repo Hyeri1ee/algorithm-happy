5952f2c7bf229ca3623eea1e8d961545e0c62af4
이혜리 Hyeri Lee <108603070+Hyeri1ee@users.noreply.github.com>
2025-04-28 21:48:41 +0900

    [Silver IV] Title: 문자열 집합, Time: 364 ms, Memory: 31192 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n,m;
+    static HashSet<String> sets = new HashSet<>();
+
+    public static void main(String[] args)throws Exception{
+        int result = 0 ;
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        while(n-- > 0){
+            String input = bf.readLine();
+            sets.add(input);
+        }
+
+        while(m-- > 0){
+            String input = bf.readLine();
+            if (sets.contains(input))
+                result++;
+        }
+
+        System.out.println(result);
+    }
+
+}
