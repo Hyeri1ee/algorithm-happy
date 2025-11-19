427e89c22c33157afd5cafefe2b928599c8f10d2
Hyeri Lee <108603070+Hyeri1ee@users.noreply.github.com>
2025-11-19 10:36:44 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 500 ms, Memory: 60036 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,45 +1,66 @@
-import java.io.*;  
-import java.util.*;  
-  
-public class Main {  
-  
-    public static void main(String[] args) throws IOException {  
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-        int n = Integer.parseInt(br.readLine());  
-        int[] parent = new int[n + 1];  
-        ArrayList<Integer>[] adj = new ArrayList[n + 1];  
-        for (int i = 1; i <= n; i++) {  
-            adj[i] = new ArrayList<>();  
-        }  
-        boolean[] visited = new boolean[n + 1];  
-        StringTokenizer st;  
-        for (int i = 1; i < n; i++) {  
-            st = new StringTokenizer(br.readLine());  
-            int a = Integer.parseInt(st.nextToken());  
-            int b = Integer.parseInt(st.nextToken());  
-            adj[a].add(b);  
-            adj[b].add(a);  
-        }  
-  
-        //BFS를 위한 큐  
-        Queue<Integer> queue = new LinkedList<>();  
-        queue.add(1);  
-        visited[1] = true;  
-while (!queue.isEmpty()) {  
-            int cur = queue.poll();  
-            for (int next : adj[cur]) {  
-                //방문!!
-                if (visited[next]) {  
-                    continue;  
-                }  
-                visited[next] = true;  
-                queue.add(next);  
-                parent[next] = cur;  
-            }  
-        }  
-  
-        for (int i = 2; i <= n; i++) {  
-            System.out.println(parent[i]);  
-        }  
-    }  
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    static int n;
+    static LinkedList<Integer>[] graph;
+    static int[] parent;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception{
+        input();
+        solve();
+    }
+    private static void solve(){
+
+        bfs(1);
+
+    }
+    private static void bfs(int start){
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n+1];
+        queue.add(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+
+            for(int next : graph[cur]){
+                if(!visited[next]){
+                    visited[next] = true;
+                    parent[next] = cur;//부모 갱신
+                    queue.add(next);
+                }
+            }
+        }
+
+        for(int i = 2 ; i <= n ; i++){
+            sb.append(parent[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+    private static void input() throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        parent = new int[n+1];
+        IntStream.range(0, parent.length)
+                .forEach(i -> parent[i] = i);
+        graph = new LinkedList[n+1];
+        for(int i = 0 ; i < graph.length ; i++){
+            graph[i] = new LinkedList<>();
+        }
+
+        StringTokenizer st;
+        for(int i = 1 ; i  < n ;i++){
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+    }
+
+    
 }
